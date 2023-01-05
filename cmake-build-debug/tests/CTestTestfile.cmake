a7634a73709658b0fbabc2d9410fdf65abfa9793
Константин <100041778+KonstantinGyr@users.noreply.github.com>
2023-01-05 19:20:56 +0400

    Delete cmake-build-debug directory

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kot/CLionProjects/FinalProject/Gyr_searchEngine/tests
-# Build directory: C:/Users/Kot/CLionProjects/FinalProject/Gyr_searchEngine/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Kot/CLionProjects/FinalProject/Gyr_searchEngine/cmake-build-debug/tests/GtestTests[1]_include.cmake")
